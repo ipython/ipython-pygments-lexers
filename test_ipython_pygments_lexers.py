065b5ef5ee39dbba4d3b62650af113a5bc5f24b9
Thomas Kluyver <thomas@kluyver.me.uk>
2025-01-15 15:22:20 +0000

    Add test of entry point names from IPython

--- test_ipython_pygments_lexers.py
+++ test_ipython_pygments_lexers.py
@@ -3,15 +3,28 @@
 # Copyright (c) IPython Development Team.
 # Distributed under the terms of the Modified BSD License.
 
+import pygments.lexers
+import pytest
 from pygments import __version__ as pygments_version
-from pygments.token import Token
 from pygments.lexers import BashLexer
+from pygments.token import Token
 
 import ipython_pygments_lexers as lexers
 
 pyg214 = tuple(int(x) for x in pygments_version.split(".")[:2]) >= (2, 14)
 
 TOKEN_WS = Token.Text.Whitespace if pyg214 else Token.Text
+
+EXPECTED_LEXER_NAMES = [
+    cls.name for cls in [lexers.IPythonConsoleLexer, lexers.IPython3Lexer]
+]
+
+
+@pytest.mark.parametrize("expected_lexer", EXPECTED_LEXER_NAMES)
+def test_pygments_entry_points(expected_lexer: str):
+    """Check whether the ``entry_points`` for ``pygments.lexers`` are correct."""
+    all_pygments_lexer_names = {l[0] for l in pygments.lexers.get_all_lexers()}
+    assert expected_lexer in all_pygments_lexer_names
 
 
 def test_plain_python():
